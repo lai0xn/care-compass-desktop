be3efc4cfa32ac12aed1f39dfd01f62ecf3d8730
9EED <88392191+9EED@users.noreply.github.com>
2024-03-02 07:13:25 +0100

    uwu

--- frontend/src/SignupPage.svelte
+++ frontend/src/SignupPage.svelte
@@ -1,23 +1,49 @@
 <script>
     import { select_page } from "./stores";
     import Title from "./Title.svelte";    
+
+    /*
+    	    name: string;
+	    lastName: string;
+	    password: string;
+	    email: string;
+	    phoneNumber: string;
+	    isDoctor: boolean;
+	    isActive: boolean;
+	    sex: boolean;
+    */
+    let new_user = {
+        name: '',
+        lastName: '',
+        password: '',
+        email: '',
+        phoneNumber: '',
+        isDoctor: true,
+        isActive: true,
+        sex: 'male',
+    }
+
+    let submit = (event) => {
+        console.table(new_user);
+    }
+        
 </script>
 <main class="frosted-panel">
     <Title title="signup"/>
     <form class="signup-form">
         <h1>Signup</h1>
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Full Name" />
-        <input type="text" placeholder="Phone Number" />
-        <input type="text" placeholder="field" />
+        <input type="text" placeholder="First Name" required bind:value={new_user.name} />
+        <input type="text" placeholder="Last Name" required bind:value={new_user.lastName} />
+        <input type="text" placeholder="Email" required bind:value={new_user.email} />
+        <input type="text" placeholder="Phone Number" required bind:value={new_user.phoneNumber} />
         <div class='input'>gender:
-            <input type="radio" name="gender" value="male"> male
-            <input type="radio" name="gender" value="female"> female
+            <input type="radio" name="gender" value="male" bind:group={new_user.sex}> male
+            <input type="radio" name="gender" value="female" bind:group={new_user.sex}> female
         </div>
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm Password" />
+        <input type="password" placeholder="Password" required bind:value={new_user.password} />
+        <!-- <input type="password" placeholder="Confirm Password" /> -->
 
-        <button type="submit">Signup</button>
+        <button type="submit" on:click={submit}>Signup</button>
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <p>Already have an account? <span on:click={()=>select_page('Login')}>Login</span></p>
     </form>
